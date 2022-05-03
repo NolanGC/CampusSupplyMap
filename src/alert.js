e8f6b870b8ac025177aa339116c0436d74aec2b9
Nolan Clement <nolangclement@gmail.com>
2022-05-02 23:22:43 -0700

    info button

--- src/alert.js
+++ src/alert.js
@@ -16,7 +16,7 @@ export default function ChakraAlert (props) {
   alignItems='center'
   justifyContent='center'
   textAlign='center'
-  height='150px'
+  height='135px'
   colorScheme='pink'
 >
   <AlertIcon boxSize='40px' mr={0} />
@@ -38,7 +38,7 @@ export default function ChakraAlert (props) {
           alignItems='center'
           justifyContent='center'
           textAlign='center'
-          height='150px'
+          height='135px'
           colorScheme='pink'
           >
           <ArrowDownIcon boxSize='60px' mr={0} />
