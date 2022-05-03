5add88a61440a56590349dad0ef14783a46c38c7
Nolan Clement <nolangclement@gmail.com>
2022-05-03 15:49:11 -0700

    complex db object

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import banner from './banner.png';
 import bannerb from './bannerb.png';
 import InfoButton from "./InfoButton";
 import 'firebase/compat/firestore';
-import { collection , onSnapshot } from 'firebase/firestore';
+import { collection , onSnapshot, doc } from 'firebase/firestore';
 import {  extendTheme, ChakraProvider, Stack } from '@chakra-ui/react'
 import {
   Alert,
@@ -20,41 +20,66 @@ import {
 export default function Example() {
 
   const [rooms, setRooms] = useState([[]]);
+  const [stocked, setStocked] = useState([[]]);
   const [message, setMessage] = useState("");
   const [title, setTitle] = useState("");
   const [display, setDisplay] = useState(false);
-  console.log(rooms);
-  
+
   useEffect(() => {
-    onSnapshot(collection(db,'rooms'),(snapshot)=>{
-      setRooms(snapshot.docs.map(doc=>doc.data()));
-      
-      })
+    onSnapshot(doc(db,'rooms', "Sgf8Tm1RSckE20WMtigL"),(snapshot)=>{
+      let data = snapshot.data();
+      let keys = Object.keys(data);
+      let dataArray = [];
+      let stockedArray = [];
+      for (const key of keys){
+        dataArray.push({id:key, ...data[key]})
+        stockedArray.push(key)
+      }
+      setRooms(dataArray);
+      setStocked(stockedArray);
+    })
   },[display]);
+  function getMapAreas(){
+    let allAreas = mapArea;
+    let valid = allAreas.filter((element, index) => stocked.includes(""+roomMap[index+1]))
+    return valid
+  }
+  function getNumber(element) {
+    return roomMap[mapArea.indexOf(element)+1]
+  }
+  function getMessage(element){
+    let message = ""
+    let roomNumber = getNumber(element)
+    for(const room of rooms){
+      if(room['id'] == roomNumber){
+        message += room['description']
+        message += room['type']
+        break;
+      }
+    }
+    return message
+  }
 
   const ImageMapComponent = React.useMemo(
     () => (
-      <body>
-      <meta name="viewport"></meta>
-      <ChakraProvider>
+      <div>
           <Stack spacing={0}>
           <Image src={banner} alt="banner" width="100%" height="100%" />
           <ChakraAlert message={message} title={title} display={display}></ChakraAlert>
           <ImageMap
               className="usage-map"
               src={map}
-              map={mapArea.filter((element, index) => JSON.stringify(rooms[0]).includes(""+roomMap[index+1]))}
-              onMapClick={(index) => {
+              map={getMapAreas()}
+              onMapClick={(element, index) => {
                 setDisplay(true)
-                setMessage(rooms[0][roomMap[mapArea.indexOf(index)+1]])
-                setTitle("Room " + roomMap[mapArea.indexOf(index)+1] + " is in stock!")
+                setMessage(getMessage(element))
+                setTitle("Room " + getNumber(element) + " is in stock!")
               }}
             />
             <Image src={bannerb} alt="banner" width="100%" height="100%" />
             <InfoButton ></InfoButton>
           </Stack>
-      </ChakraProvider>
-      </body>
+      </div>
     ),
     [rooms, message]
   );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import map from './map.png';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ChakraProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
